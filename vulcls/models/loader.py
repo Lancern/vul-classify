61c87e014cf0c2d5cddafe184460d473bd097840
Lancern <msrlancern@126.com>
2019-09-08 16:29:22 +0800

    Bug fixes in model loading modules.

--- vulcls/models/loader.py
+++ vulcls/models/loader.py
@@ -4,16 +4,16 @@ import importlib.util
 from .base import AbstractModel
 
 
-def _load_module_file(file_name) -> Any:
-    spec = importlib.util.spec_from_file_location(file_name, file_name)
+def _load_module_file(file_name: str, module_name: str) -> Any:
+    spec = importlib.util.spec_from_file_location(module_name, file_name)
     module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(module)
     return module
 
 
-def load_model_object(py_file: str, model_name: str) -> AbstractModel:
-    module = _load_module_file(py_file)
-    model_cls = module[model_name]
+def load_model_object(py_file: str, module_name: str, model_name: str) -> AbstractModel:
+    module = _load_module_file(py_file, module_name)
+    model_cls = getattr(module, model_name)
     return model_cls()
 
 
